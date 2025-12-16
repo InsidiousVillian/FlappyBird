849c81e1c0859834c1ce36fd83c67657a7f7820d
Jet Kingston <jettaikingston10@gmail.com>
2025-12-16 18:26:50 +0200

    [Addition]<Obstacles now work and on overlap with player display log>

---
+++ Source/FlappyBird/Public/Obstacles/Obstacles.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Obstacles.generated.h"
+
+UCLASS()
+class FLAPPYBIRD_API AObstacles : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AObstacles();
+
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	
+	UFUNCTION(BlueprintCallable)
+	void OnHitPlayer(ACharacter* PlayerRef);
+
+};
