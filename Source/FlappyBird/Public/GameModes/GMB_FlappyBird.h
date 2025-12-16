6d471d059bee0de3689096e670adacf3a1814d04
Jet Kingston <jettaikingston10@gmail.com>
2025-12-16 10:36:27 +0200

    [Phase 1]<Folder structure created, game mode base created and test map>

---
+++ Source/FlappyBird/Public/GameModes/GMB_FlappyBird.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GMB_FlappyBird.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FLAPPYBIRD_API AGMB_FlappyBird : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
